626fd936ca4103ba763a84651363f6a5474cd549
bujarr <bujarskola@gmail.com>
2020-05-20 18:52:42 +0200

    Error handling on Add and Remove books, many improvements, added a book list, book added now get added onto arrListBook

--- src/sample/EmployerMenuController.java
+++ src/sample/EmployerMenuController.java
@@ -7,8 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -27,6 +26,9 @@ public class EmployerMenuController implements Initializable {
     private TextField remQuantityTextField;
 
     @FXML
+    private Label remQuantityLabel;
+
+    @FXML
     private TextField quantityTextField;
 
     @FXML
@@ -38,9 +40,23 @@ public class EmployerMenuController implements Initializable {
     @FXML
     private TextField genreTextField;
 
+    @FXML
+    private CheckBox remQuantityCheckBox;
+
+    @FXML
+    private TextArea bookListTextArea;
+
+    @FXML
+    private Button addBookBtn;
+
+    @FXML
+    private Button remBookBtn;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        for (int i = 0; i < StoreController.arrListBooks.size(); i++) {
+            bookListTextArea.insertText(0, StoreController.arrListBooks.get(i).toString() + "\n");
+        }
     }
 
     SQLConnector sql = new SQLConnector();
@@ -57,18 +73,33 @@ public class EmployerMenuController implements Initializable {
     }
 
     @FXML
-    public void addBook() {
+    public void addBook(ActionEvent event) {
         Alert alert = new Alert(null);
 
-        if (hasText()) {
+
+        if (hasText(event)) {
             String title = titleTextField.getText();
             String author = authorTextField.getText();
             String genre = genreTextField.getText();
             int quantity = Integer.parseInt(quantityTextField.getText());
             double price = Double.parseDouble(priceTextField.getText());
 
-            Book book = new Book (null, title, author, genre, quantity, price);
+            Book book = new Book(null, title, author, genre, quantity, price);
+            int id = 0;
 
+            for (int i = 0; i < StoreController.arrListBooks.size(); i++) {
+
+                System.out.println("VALUE OF ID IS: " + id);
+
+                if (id <= StoreController.arrListBooks.get(i).getId()) {
+                    id = StoreController.arrListBooks.get(i).getId() + 1;
+                    System.out.println("ID HAS BEEN CHANGED TO: " + id);
+                }
+            }
+
+            book.setId(id);
+
+            StoreController.arrListBooks.add(book);
 
             sql.addBook(book);
             alert.setAlertType(Alert.AlertType.INFORMATION);
@@ -81,47 +112,99 @@ public class EmployerMenuController implements Initializable {
             genreTextField.clear();
             quantityTextField.clear();
             priceTextField.clear();
-
-        } else {
-
-            alert.setAlertType(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setContentText("Please make sure all of the text fields have text in them.");
-            alert.show();
-        }
-
-
-    }
-
-    public void removeBook() {
-        int remId = Integer.parseInt(bookIdTextField.getText());
-        Alert alert = new Alert(null);
-        Book book = new Book(null, null, null, null, null, remId);
-        book.setId(remId);
-
-        if (!bookIdTextField.getText().isEmpty()) {
-            sql.removeBook(book);
-            alert.setAlertType(Alert.AlertType.INFORMATION);
-            alert.setTitle("Success");
-            alert.setContentText("The book with bookid " + bookIdTextField.getText() + " has been removed from the database!");
-            alert.show();
             bookIdTextField.clear();
+            bookListTextArea.clear();
 
+            for (int i = 0; i < StoreController.arrListBooks.size(); i++) {
+                bookListTextArea.insertText(0, StoreController.arrListBooks.get(i).toString() + "\n");
+            }
 
         } else {
+
             alert.setAlertType(Alert.AlertType.ERROR);
             alert.setTitle("Error");
             alert.setContentText("Please make sure all of the text fields have text in them.");
             alert.show();
         }
+
+
+    }
+
+    @FXML
+    public void removeBook(ActionEvent event) {
+        Alert alert = new Alert(null);
+        if (hasText(event)) {
+            boolean removeEntireBook = true;
+
+            if (remQuantityCheckBox.isSelected()) {
+                removeEntireBook = false;
+                remQuantityLabel.setVisible(true);
+                remQuantityTextField.setVisible(true);
+                System.out.println("Don't remove entire book.");
+            }
+
+            if (removeEntireBook) {
+                try {
+                    System.out.println("Remove entire book.");
+                    int remId = Integer.parseInt(bookIdTextField.getText());
+
+                    Book book = new Book(null, null, null, null, null, remId);
+                    book.setId(remId);
+
+
+                    if (!bookIdTextField.getText().isEmpty()) {
+                        sql.removeBook(book);
+                        for (int i = 0; i < StoreController.arrListBooks.size(); i++) {
+                            if (StoreController.arrListBooks.get(i).getId() == remId) {
+                                System.out.println("Book " + StoreController.arrListBooks.get(i).getTitle() + " with BookID: "
+                                        + StoreController.arrListBooks.get(i).getId() + " has been removed from arrListBooks.");
+                                StoreController.arrListBooks.remove(i);
+                            }
+                        }
+
+                        alert.setAlertType(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Success");
+                        alert.setContentText("The book with bookid " + bookIdTextField.getText() + " has been removed from the database!");
+                        alert.show();
+                        bookIdTextField.clear();
+                        bookListTextArea.clear();
+
+                        for (int i = 0; i < StoreController.arrListBooks.size(); i++) {
+                            bookListTextArea.insertText(0, StoreController.arrListBooks.get(i).toString() + "\n");
+                        }
+
+                    } else {
+                        alert.setAlertType(Alert.AlertType.ERROR);
+                        alert.setTitle("Error");
+                        alert.setContentText("Please make sure all of the text fields have text in them.");
+                        alert.show();
+                    }
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                    alert.setAlertType(Alert.AlertType.ERROR);
+                    alert.setTitle("Failure");
+                    alert.setContentText("Please enter a number instead of text.");
+                    alert.show();
+                }
+            } else {
+                System.out.println("Remove only part.");
+            }
+        }
     }
 
 
-    public boolean hasText() {
-        if (!titleTextField.getText().isEmpty() && !authorTextField.getText().isEmpty() && !genreTextField.getText().isEmpty() && !quantityTextField.getText().isEmpty() && !priceTextField.getText().isEmpty()) {
+    public boolean hasText(ActionEvent event) {
+        if (event.getSource().toString().contains("addBookBtn")) {
+            if (!titleTextField.getText().isEmpty() && !authorTextField.getText().isEmpty() && !genreTextField.getText().isEmpty() && !quantityTextField.getText().isEmpty() && !priceTextField.getText().isEmpty()) {
 
-            System.out.println("All textfields have text.");
-            return true;
+                System.out.println("All textfields have text.");
+                return true;
+            }
+        } else if (event.getSource().toString().contains("remBookBtn")) {
+            if (!bookIdTextField.getText().isEmpty()) {
+                System.out.println("Book id field has text.");
+                return true;
+            }
         }
 
         System.out.println("They don't have text.");
